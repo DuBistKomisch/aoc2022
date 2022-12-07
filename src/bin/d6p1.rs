9d28dcf3aeb1f999938f752138781316e5501cde
Jake Barnes <me@jakebarn.es>
2022-12-07 13:55:44 +1100

    Day 6

---
+++ src/bin/d6p1.rs
@@ -0,0 +1,14 @@
+fn main() -> std::io::Result<()> {
+    let mut lines = std::io::stdin().lines();
+    if let Some(line) = lines.next() {
+        let chars: Vec<char> = line?.chars().collect();
+        let offset = 4 + chars.windows(4).position(|quad| {
+            let mut vec = quad.to_vec();
+            vec.sort();
+            vec.dedup();
+            vec.len() == 4
+        }).unwrap();
+        println!("{}", offset);
+    }
+    Ok(())
+}
---
+++ src/bin/d6p2.rs
@@ -0,0 +1,14 @@
+fn main() -> std::io::Result<()> {
+    let mut lines = std::io::stdin().lines();
+    if let Some(line) = lines.next() {
+        let chars: Vec<char> = line?.chars().collect();
+        let offset = 14 + chars.windows(14).position(|quad| {
+            let mut vec = quad.to_vec();
+            vec.sort();
+            vec.dedup();
+            vec.len() == 14
+        }).unwrap();
+        println!("{}", offset);
+    }
+    Ok(())
+}
