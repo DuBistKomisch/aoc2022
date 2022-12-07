09e06297b3a9f3ac0c292fbd053aa41e62a3a2ce
Jake Barnes <me@jakebarn.es>
2022-12-07 11:34:02 +1100

    Day 4

---
+++ src/bin/d4p2.rs
@@ -0,0 +1,13 @@
+fn main() -> std::io::Result<()> {
+    let mut count: u32 = 0;
+    for line in std::io::stdin().lines() {
+        let parts: Vec<u32> = line?.split(&['-', ',']).map(|n| n.parse().unwrap()).collect();
+        if let [a1, a2, b1, b2] = &parts[..] {
+            if b2 >= a1 && b1 <= a2 {
+                count += 1;
+            }
+        }
+    }
+    println!("{}", count);
+    Ok(())
+}
